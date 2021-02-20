74219fe351f4e42a8e38a2ae875d29dc4a06594a
desertsniper87 <torshobuet@gmail.com>
2021-02-20 20:03:23 +0600

    subpage scrape init

--- conscrap/spiders/laws_spider.py
+++ conscrap/spiders/laws_spider.py
@@ -3,10 +3,11 @@ import re
 import scrapy
 
 class LawsSpider(scrapy.Spider):
+    BASE_URL = 'http://bdlaws.minlaw.gov.bd'
     name = "laws"
 
     def start_requests(self):
-        urls = ['http://bdlaws.minlaw.gov.bd/act-957.html']
+        urls = [ self.BASE_URL + '/act-957.html']
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
@@ -16,5 +17,12 @@ class LawsSpider(scrapy.Spider):
             law_title = law.xpath("text()").get().strip()
             law_link = law.xpath("@href").get().strip()
 
+            law_url = self.BASE_URL + law_link
+            law_description = self.parse_subpage(law_url)
+
+
             print(law_title, law_link)
+
+    def parse_subpage(self, page_url):
+        pass
 
